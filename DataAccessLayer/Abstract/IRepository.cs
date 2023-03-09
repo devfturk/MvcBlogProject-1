00ebc51116d2922f893b02f4090558c120056794
devfturk <furkanturk0@outlook.com.tr>
2023-03-09 12:10:52 +0300

    DataAccessLayer Katmanına başlanıldı. Context Sınıfı, ConString yazıldı ve migrations yapılandırılması yapıldı. Db diagram oluşturulduktan sonra IRepository sınıfı oluşturuldu.

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+        List<TEntity> GetAll();
+        int Insert(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+        TEntity GetByID(int id);
+
+    }
+}
