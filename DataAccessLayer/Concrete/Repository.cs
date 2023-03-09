94f198d19c1b1245cc5b3647e4f4a9cedd4722ad
devfturk <furkanturk0@outlook.com.tr>
2023-03-09 14:23:39 +0300

    DataAccessLayer,BusinessLayer ve MVC controller View yapılarına eklemeler yapıldı. MVC ve katmanlı mimari için basit seviye temel oluşturuldu.

---
+++ DataAccessLayer/Concrete/Repository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        Context context = new Context();
+        DbSet<T> _object;
+
+        public Repository()
+        {
+            _object = context.Set<T>();
+        }
+
+        public int Delete(T entity)
+        {
+            _object.Remove(entity);
+            return context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public T GetByID(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public int Insert(T entity)
+        {
+            _object.Add(entity);
+            return context.SaveChanges();
+        }
+
+        public int Update(T entity)
+        {
+
+            return context.SaveChanges();
+        }
+    }
+}
---
+++ MvcBlogProject-1/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlogProject_1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager();
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categories = categoryManager.GetAll();
+            return View(categories);
+        }
+    }
+}
